71e0b02418253d8f5ff3828fd0c944adcd87d186
Benedict <benedictcontawe@gmail.com>
2020-03-21 19:39:30 +0800

    Update View and Main Class for having 3 new functionality

--- app/src/main/java/com/example/benedict/ConnectionApp/MainActivity.java
+++ app/src/main/java/com/example/benedict/ConnectionApp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.benedict.ConnectionApp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.graphics.Rect;
 import android.media.MediaRecorder;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private MediaRecorder mediaRecorder;
 
     private TextView txtDuration;
-    private ImageView imgSignal,imgMicrophone;
+    private ImageView imgSignal,imgMicrophone, imgPlay, imgStop, imgRefresh;
 
     private Rect rect;
 
@@ -38,16 +39,26 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         init();
         //TODO: imgSignal - should be equal to the input source of sound in the microphone
+        //TODO: imgPlay - should play the recorded audio in the microphone
+        //TODO: imgStop - should stop the recorded audio in the microphone
+        //TODO: imgRefresh - should go back to on record state
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void init() {
         txtDuration = (TextView) findViewById(R.id.text_view_duration);
         imgSignal = (ImageView) findViewById(R.id.image_view_signal);
+        imgRefresh = (ImageView) findViewById(R.id.image_view_refresh);
+        imgPlay = (ImageView) findViewById(R.id.image_view_play);
+        imgStop = (ImageView) findViewById(R.id.image_view_stop);
         imgMicrophone = (ImageView) findViewById(R.id.image_view_microphone);
 
         imgMicrophone.setOnTouchListener(this);
+        imgRefresh.setOnTouchListener(this);
+        imgPlay.setOnTouchListener(this);
+        imgStop.setOnTouchListener(this);
 
-        premitRecordAudio();
+        permissionRecordAudio();
     }
 
     @Override
@@ -59,14 +70,22 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 // Write your code to perform an action on down
                 rect = new Rect(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
                 if (view.getId() == imgMicrophone.getId()) {
-                    Log.d("onTouch","ACTION_DOWN");
+                    Log.d("onTouch","ACTION_DOWN imgMicrophone");
                     startRecord();
+                } else if (view.getId() == imgRefresh.getId()) {
+                    Log.d("onTouch","ACTION_DOWN imgRefresh");
+                }
+                else if (view.getId() == imgPlay.getId()) {
+                    Log.d("onTouch","ACTION_DOWN imgPlay");
+                }
+                else if (view.getId() == imgStop.getId()) {
+                    Log.d("onTouch","ACTION_DOWN imgStop");
                 }
                 return true;
             case MotionEvent.ACTION_MOVE:
                 // Write your code to perform an action on continuous touch move
                 if (view.getId() == imgMicrophone.getId()) {
-                    Log.d("onTouch","ACTION_MOVE");
+                    Log.d("onTouch","ACTION_MOVE imgMicrophone");
                     startRecord();
                 }
                 return true;
@@ -78,6 +97,15 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                     // User moved inside bounds
                     if (view.getId() == imgMicrophone.getId()) {
 
+                    } else if (view.getId() == imgRefresh.getId()) {
+                        Log.d("onTouch","imgRefresh");
+                        setRecordVisibility();
+                    }
+                    else if (view.getId() == imgPlay.getId()) {
+                        Log.d("onTouch","imgPlay");
+                    }
+                    else if (view.getId() == imgStop.getId()) {
+                        Log.d("onTouch","imgStop");
                     }
                     return true;
                 }
@@ -86,7 +114,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         return false;
     }
 
-    private void premitRecordAudio() {
+    private void permissionRecordAudio() {
+        //TODO: ake the code clear
         if (
             ActivityCompat.checkSelfPermission(
                     this,
@@ -111,9 +140,37 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
     }
 
+    private void startRecord() {
+        if (imgMicrophone.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.ic_microphone_normal).getConstantState()) {
+            startAsyncTask();
+            startMediaRecorder();
+        }
+    }
+
+    private void resetRecord() {
+        if (imgMicrophone.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.ic_microphone_pressed).getConstantState()) {
+            stopMediaRecorder();
+            resetRecordState();
+        }
+    }
+
+    private String getFilePath() {
+        String filepath;
+        filepath = Environment.getExternalStorageDirectory().getPath();
+        //filepath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        filepath += "/MediaRecorderSample.3gp"; //AudioRecording.3gp
+
+
+        //File file = new File(filepath, "MediaRecorderSample.3gp");
+        //if (!file.exists()) file.mkdirs(); //Make a new Folder
+
+        Log.d("startMediaRecorder", "getFilePath() " + filepath);
+        return (filepath);
+    }
+
     private void startAsyncTask() {
-        TimerAsyncTask timerAsyncTask = new TimerAsyncTask(this);
-        timerAsyncTask.execute(10);
+        RecordTimerAsyncTask recordTimerAsyncTask = new RecordTimerAsyncTask(this);
+        recordTimerAsyncTask.execute(1);
     }
 
     private void startMediaRecorder() {
@@ -143,39 +200,11 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
     }
 
-    private String getFilePath() {
-        String filepath;
-        filepath = Environment.getExternalStorageDirectory().getPath();
-        //filepath = Environment.getExternalStorageDirectory().getAbsolutePath();
-        filepath += "/MediaRecorderSample.3gp"; //AudioRecording.3gp
-
-
-        //File file = new File(filepath, "MediaRecorderSample.3gp");
-        //if (!file.exists()) file.mkdirs(); //Make a new Folder
-
-        Log.d("startMediaRecorder", "getFilePath() " + filepath);
-        return (filepath);
-    }
-
-    private void startRecord() {
-        if (imgMicrophone.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.ic_microphone_normal).getConstantState()) {
-            startAsyncTask();
-            startMediaRecorder();
-        }
-    }
-
-    private void resetRecord() {
-        stopMediaRecorder();
-        txtDuration.setText("00:00");
-        txtDuration.setTextColor(ContextCompat.getColor(this,R.color.black));
-        imgMicrophone.setImageResource(R.drawable.ic_microphone_normal);
-    }
-
-    private class TimerAsyncTask extends AsyncTask<Integer, Integer, String> {
+    private class RecordTimerAsyncTask extends AsyncTask<Integer, Integer, String> {
         int counter;
         private WeakReference<MainActivity> activityWeakReference;
 
-        TimerAsyncTask(MainActivity activity) {
+        RecordTimerAsyncTask(MainActivity activity) {
             activityWeakReference = new WeakReference<MainActivity>(activity);
         }
 
@@ -187,7 +216,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             if (activity == null || activity.isFinishing()) {
                 return;
             }
-
             counter = 0;
             imgMicrophone.setImageResource(R.drawable.ic_microphone_pressed);
             txtDuration.setTextColor(ContextCompat.getColor(getBaseContext(),R.color.green));
@@ -196,7 +224,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         @Override
         protected String doInBackground(Integer... integers) {
             Log.d("TimerAsyncTask","doInBackground() background thread task");
-
             while (imgMicrophone.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.ic_microphone_pressed).getConstantState()) {
                 publishProgress(counter);
                 try {
@@ -207,6 +234,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                     Log.e("TimerAsyncTask","Error " + e.getMessage());
                 }
             }
+
             return "Finished!";
         }
 
@@ -230,9 +258,29 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             if (activity == null || activity.isFinishing()) {
                 return;
             }
-
+            setPlayVisibility();
             Toast.makeText(activity, string, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void resetRecordState() {
+        txtDuration.setText("00:00");
+        txtDuration.setTextColor(ContextCompat.getColor(this,R.color.black));
+        imgMicrophone.setImageResource(R.drawable.ic_microphone_normal);
+    }
+
+    private void setRecordVisibility() {
+        imgRefresh.setVisibility(View.INVISIBLE);
+        imgMicrophone.setVisibility(View.VISIBLE);
+        imgPlay.setVisibility(View.INVISIBLE);
+        imgStop.setVisibility(View.INVISIBLE);
+    }
+
+    private void setPlayVisibility() {
+        imgRefresh.setVisibility(View.VISIBLE);
+        imgMicrophone.setVisibility(View.INVISIBLE);
+        imgPlay.setVisibility(View.VISIBLE);
+        imgStop.setVisibility(View.INVISIBLE);
     }
 
     @Override
