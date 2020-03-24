9f7b425e13bedeb0f4abd7f296d4dfc7bd25b021
Benedict <benedictcontawe@gmail.com>
2020-03-24 16:34:32 +0800

    Adding Todo in the main class

--- app/src/main/java/com/example/benedict/ConnectionApp/MainActivity.java
+++ app/src/main/java/com/example/benedict/ConnectionApp/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         init();
         ManifestPermission.newInstance(this);
         //TODO: imgSignal - should be equal to the input source of sound in the microphone
+        //TODO: imgRefresh - when clicked delete the recorded audio MediaRecorderSample.3gp
     }
 
     @SuppressLint("ClickableViewAccessibility")
