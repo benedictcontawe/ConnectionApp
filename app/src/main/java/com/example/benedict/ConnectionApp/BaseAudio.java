8e116ecd35a251d31ca2b14a4d6e72a7cd47d517
Benedict <benedictcontawe@gmail.com>
2020-03-25 12:25:49 +0800

    Update Base Audio and add method.

--- app/src/main/java/com/example/benedict/ConnectionApp/BaseAudio.java
+++ app/src/main/java/com/example/benedict/ConnectionApp/BaseAudio.java
@@ -1,12 +1,24 @@
 package com.example.benedict.ConnectionApp;
 
+import android.media.MediaRecorder;
 import android.os.Environment;
 import android.util.Log;
 
 abstract class BaseAudio {
 
+    public static MediaRecorder setMediaRecorderInstance() {
+        MediaRecorder mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC); //Use Android Microphone
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP); //.3gp
+
+        //mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB); //For Low Quality
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB); //For High Quality
+
+        return mediaRecorder;
+    }
+
     public static String getFilePath() {
-        Log.d("AudioRecorder","getFilePath()");
+        Log.d("BaseAudio","getFilePath()");
         String filepath;
         filepath = Environment.getExternalStorageDirectory().getPath();
         //filepath = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -17,5 +29,10 @@ abstract class BaseAudio {
 
         Log.d("AudioRecorder", "getFilePath() " + filepath);
         return (filepath);
+    }
+
+    public static String getDevNullPath() {
+        Log.d("BaseAudio","getFilePath()");
+        return "/dev/null";
     }
 }
