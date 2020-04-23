39cd3e521d0c47a12629c0447450a4ea22464eaf
Benedict <benedictcontawe@gmail.com>
2020-04-23 19:39:51 +0800

    Create Manifest Permission Object for easy to check permission and get easy result permission

--- app/src/main/java/com/example/benedict/simstate/MainActivity.kt
+++ app/src/main/java/com/example/benedict/simstate/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.example.benedict.simstate
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
-        private const val PERMISSION_STATE = 0
+        private val TAG : String = MainActivity::class.java.getSimpleName()
     }
-
-
-    private var callPermissionSettings: Boolean? = null
-    private var requestGranted: Boolean? = null
-    private var neverAskAgain: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //region Check Permission On Click Listener
+        allCheckPermissions.setOnClickListener(this)
+        telephonyCheckPermissions.setOnClickListener(this)
+        microphoneCheckPermissions.setOnClickListener(this)
+        cameraCheckPermissions.setOnClickListener(this)
+        videoCallCheckPermissions.setOnClickListener(this)
+        galleryCheckPermissions.setOnClickListener(this)
+        //endregion
+        //region Request Permission On Click Listener
+        allRequestPermissions.setOnClickListener(this)
+        telephonyRequestPermissions.setOnClickListener(this)
+        microphoneRequestPermissions.setOnClickListener(this)
+        cameraRequestPermissions.setOnClickListener(this)
+        videoCallRequestPermissions.setOnClickListener(this)
+        galleryRequestPermissions.setOnClickListener(this)
+        //endregion
+        showRationalDialog.setOnClickListener(this)
     }
 
     override fun onPause() {
@@ -36,135 +40,144 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        requestPermissions()
     }
 
-    private fun checkPermission(manifestPermissions : Array<String>) : Boolean {
-        return manifestPermissions.filter { manifestPermission ->
-            !isPermissionsGranted(manifestPermission)
-        }.isNotEmpty()
-        /*
-        return if (requestGranted == null || neverAskAgain == null) {
-            true
-        } else if (!requestGranted!! && !neverAskAgain!!) {
-            txtData.text = "All permission denied"
-            true
-        } else if (neverAskAgain as Boolean && !requestGranted!!) {
-            txtData.text = "All permission denied and set to Don't Ask Again"
-            false
-        } else {
-            txtData.text = "All permission allowed"
-            false
-        }
-        */
-    }
-
-    private fun checkAppPermissionSettings() : Boolean {
-        return if (!callPermissionSettings!! && neverAskAgain!! && !requestGranted!!) {
-            callPermissionSettings = true
-            true
-        } else {
-            false
-        }
-    }
-
-    private fun requestPermissions() {
-        Log.d("PermissionsResult", "requestPermissions()")
-        val manifestPermissions: Array<String> =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                arrayOf(
-                    Manifest.permission.READ_PHONE_STATE,
-                    Manifest.permission.READ_CONTACTS,
-                    Manifest.permission.WRITE_CONTACTS,
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                )
-            } else {
-                arrayOf(
-                    Manifest.permission.READ_PHONE_STATE,
-                    Manifest.permission.READ_CONTACTS,
-                    Manifest.permission.WRITE_CONTACTS,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+    override fun onClick(view : View) {
+        when(view) {
+            allCheckPermissions -> {
+                ManifestPermission.checkSelfPermission(this,
+                    ManifestPermission.allPermissions,
+                    isGranted = { setTextData("All Permissions Granted!") },
+                    isDenied = { setTextData("All Permissions Denied!") }
                 )
             }
-
-        if (checkPermission(manifestPermissions)) {
-            ActivityCompat.requestPermissions(
-                this,
-                manifestPermissions,
-                PERMISSION_STATE
-            )
+            telephonyCheckPermissions -> {
+                ManifestPermission.checkSelfPermission(this,
+                    ManifestPermission.telephonyPermissions,
+                    isGranted = { setTextData("Telephony Permissions Granted!") },
+                    isDenied = { setTextData("Telephony Permissions Denied!") }
+                )
+            }
+            microphoneCheckPermissions -> {
+                ManifestPermission.checkSelfPermission(this,
+                    ManifestPermission.microphonePermission,
+                    isGranted = { setTextData("Microphone Permissions Granted!") },
+                    isDenied = { setTextData("Microphone Permissions Denied!") }
+                )
+            }
+            cameraCheckPermissions -> {
+                ManifestPermission.checkSelfPermission(this,
+                    ManifestPermission.cameraPermission,
+                    isGranted = { setTextData("Camera Permissions Granted!") },
+                    isDenied = { setTextData("Camera Permissions Denied!") }
+                )
+            }
+            videoCallCheckPermissions -> {
+                ManifestPermission.checkSelfPermission(this,
+                    ManifestPermission.videoCallPermission,
+                    isGranted = { setTextData("Video Call Permissions Granted!") },
+                    isDenied = { setTextData("Video Call Permissions Denied!") }
+                )
+            }
+            galleryCheckPermissions -> {
+                ManifestPermission.checkSelfPermission(this,
+                    ManifestPermission.galleryPermissions,
+                    isGranted = { setTextData("Gallery Permissions Granted!") },
+                    isDenied = { setTextData("Gallery Permissions Denied!") }
+                )
+            }
+            allRequestPermissions -> {
+                ManifestPermission.requestPermissions(this,
+                    ManifestPermission.allPermissions,
+                    ManifestPermission.ALL_PERMISSION_CODE
+                )
+            }
+            telephonyRequestPermissions -> {
+                ManifestPermission.requestPermissions(this,
+                    ManifestPermission.telephonyPermissions,
+                    ManifestPermission.TELEPHONY_PERMISSION_CODE
+                )
+            }
+            microphoneRequestPermissions -> {
+                ManifestPermission.requestPermissions(this,
+                    ManifestPermission.microphonePermission,
+                    ManifestPermission.MICROPHONE_PERMISSION_CODE
+                )
+            }
+            cameraRequestPermissions -> {
+                ManifestPermission.requestPermissions(this,
+                    ManifestPermission.cameraPermission,
+                    ManifestPermission.CAMERA_PERMISSION_CODE
+                )
+            }
+            videoCallRequestPermissions -> {
+                ManifestPermission.requestPermissions(this,
+                    ManifestPermission.videoCallPermission,
+                    ManifestPermission.VIDEO_CALL_PERMISSION_CODE
+                )
+            }
+            galleryRequestPermissions -> {
+                ManifestPermission.requestPermissions(this,
+                    ManifestPermission.galleryPermissions,
+                    ManifestPermission.GALLARY_PERMISSION_CODE
+                )
+            }
+            showRationalDialog -> {
+                ManifestPermission.showRationalDialog(this,"Go to App Permission Settings?")
+            }
         }
     }
 
-    private fun isPermissionsGranted(permission : String) : Boolean {
-        return ActivityCompat
-            .checkSelfPermission(
-                application,
-                permission
-            ) == PackageManager.PERMISSION_GRANTED
+    private fun setTextData(text : String) {
+        txtData.setText(text)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //region Logs for onRequestPermissionsResult parameters
         Log.d("PermissionsResult", "requestCode $requestCode")
         Log.d("PermissionsResult","permissions " + permissions.contentToString())
         Log.d("PermissionsResult","grantResults " + grantResults.contentToString())
-        //endregion
-        //region Code for requestCode and grantResults
-        if (PERMISSION_STATE == requestCode) {
-            requestGranted = grantResults.filter { grantResult ->
-                grantResult == PackageManager.PERMISSION_DENIED
-            }.isEmpty()
-        }
-        //endregion
-        //region Code for permissions
-        neverAskAgain = true
-        for (permission in permissions) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-                Log.d("PermissionsResult", "permission Denied $permission")
-                neverAskAgain = false
-            } else {
-                if (isPermissionsGranted(permission)) {
-                    Log.d("PermissionsResult", "permission Allowed $permission")
-                    neverAskAgain = false
-                } else {
-                    Log.d(
-                        "PermissionsResult",
-                        "permission set to never ask again $permission"
-                    )
-                    //neverAskAgain = true;
-                }
-            }
-        }
-        //endregion
-        //region Code for showAppPermissionSettings
-        callPermissionSettings = false
-        if (checkAppPermissionSettings()) showAppPermissionSettings()
-        //endregion
-        //region Logs for Booleans of requestGranted, neverAskAgain, callPermissionSettings
-        Log.d("PermissionsResult", "requestGranted $requestGranted")
-        Log.d("PermissionsResult", "neverAskAgain $neverAskAgain")
-        Log.d("PermissionsResult", "callPermissionSettings $callPermissionSettings")
-        //endregion
-    }
 
-    private fun showAppPermissionSettings() {
-        Log.d("PermissionsResult", "showAppPermissionSettings()")
-        Toast.makeText(this, "Hardware Permissions Disabled", Toast.LENGTH_LONG).show()
-        val intent = Intent(
-            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-            Uri.fromParts("package", packageName, null)
+        permissions.map {permission ->
+            ManifestPermission.checkNeverAskAgain(this,permission,
+                isNeverAskAgain = {
+                    Log.d("PermissionsResult", "checkNeverAskAgain isNeverAskAgain single $permission")
+                },
+                isNotNeverAskAgain = {
+                    Log.d("PermissionsResult", "checkNeverAskAgain isNotNeverAskAgain single $permission")
+                }
+            )
+
+            ManifestPermission.checkPermissionsResult(this,permission,
+                isNeverAskAgain = {
+                    Log.d("PermissionsResult", "checkPermissionsResult isNeverAskAgain single $permission")
+                }, isDenied = {
+                    Log.d("PermissionsResult","checkPermissionsResult isDenied single $permission")
+                }, isGranted = {
+                    Log.d("PermissionsResult","checkPermissionsResult isGranted single $permission")
+                }
+            )
+        }
+
+        ManifestPermission.checkNeverAskAgain(this,permissions,
+            isNeverAskAgain = {
+                Log.d("PermissionsResult", "checkNeverAskAgain isNeverAskAgain")
+            },
+            isNotNeverAskAgain = {
+                Log.d("PermissionsResult", "checkNeverAskAgain isNotNeverAskAgain")
+            }
         )
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-        startActivity(intent)
+
+        ManifestPermission.checkPermissionsResult(this, permissions, grantResults,
+            isNeverAskAgain =  {
+                Log.d("PermissionsResult", "checkPermissionsResult isNeverAskAgain")
+            }, isDenied = {
+                Log.d("PermissionsResult","checkPermissionsResult isDenied")
+            }, isGranted = {
+                Log.d("PermissionsResult","checkPermissionsResult isGranted")
+            }
+        )
+
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/example/benedict/simstate/ManifestPermission.kt
@@ -0,0 +1,194 @@
+package com.example.benedict.simstate
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object ManifestPermission {
+
+    private val TAG = ManifestPermission::class.java.simpleName
+
+    const val ALL_PERMISSION_CODE = 1001
+    const val TELEPHONY_PERMISSION_CODE = 1002
+    const val MICROPHONE_PERMISSION_CODE = 1003
+    const val CAMERA_PERMISSION_CODE = 1004
+    const val VIDEO_CALL_PERMISSION_CODE = 1005
+    const val GALLARY_PERMISSION_CODE = 1006
+
+    val allPermissions =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        arrayOf(
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_NUMBERS
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
+    val telephonyPermissions =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        arrayOf(
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.READ_PHONE_NUMBERS
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_SMS
+        )
+    }
+
+    val microphonePermission = arrayOf(
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    val cameraPermission = arrayOf(
+        Manifest.permission.CAMERA
+    )
+
+    val videoCallPermission = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CAMERA
+    )
+
+    val galleryPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
+    fun checkSelfPermission(context : Context, permission : String, isGranted : () -> Unit = {}, isDenied : () -> Unit = {}) {
+        Log.d(TAG,"checkSelfPermission($context,$permission,isGranted(),isDenied())")
+        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG,"allGranted()")
+            isGranted()
+        }
+        else {
+            Log.d(TAG,"denied()")
+            isDenied()
+        }
+    }
+
+    fun checkSelfPermission(context : Context, permissions : Array<String>, isGranted: () -> Unit = {}, isDenied: () -> Unit = {}) {
+        Log.d(TAG,"checkSelfPermission($context,${permissions.contentToString()},isGranted(),isDenied())")
+        if (permissions.filter { permission -> ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED}.isEmpty()) {
+            Log.d(TAG,"allGranted()")
+            isGranted()
+        }
+        else {
+            Log.d(TAG,"denied()")
+            isDenied()
+        }
+    }
+
+    fun requestPermissions(activity : Activity, permissions : Array<String>, requestCode : Int) {
+        Log.d(TAG,"requestPermissions($activity,${permissions.contentToString()},$requestCode")
+        ActivityCompat.requestPermissions(activity, permissions,requestCode)
+    }
+
+    fun checkNeverAskAgain(activity : Activity, permission : String, isNeverAskAgain : () -> Unit = {}, isNotNeverAskAgain : () -> Unit = {}) {
+        Log.d(TAG,"hasPermissions($activity,$permission,isNeverAskAgain(),isNotNeverAskAgain())")
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission) && ActivityCompat.checkSelfPermission(activity,permission) == PackageManager.PERMISSION_DENIED) {
+            isNeverAskAgain()
+        } else {
+            isNotNeverAskAgain()
+        }
+    }
+
+    fun checkNeverAskAgain(activity : Activity, permissions : Array<String>, isNeverAskAgain : () -> Unit = {}, isNotNeverAskAgain : () -> Unit = {}) {
+        Log.d(TAG,"hasPermissions($activity,${permissions.contentToString()},isNeverAskAgain(),isNotNeverAskAgain())")
+        if(permissions.filter { permission -> ActivityCompat.shouldShowRequestPermissionRationale(activity, permission) && ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_DENIED }.none()) {
+            Log.d(TAG,"isNeverAskAgain()")
+            isNeverAskAgain()
+        }
+        else {
+            Log.d(TAG,"isNotNeverAskAgain()")
+            isNotNeverAskAgain()
+        }
+    }
+
+    fun checkPermissionsResult(activity : Activity, permission : String, isNeverAskAgain : () -> Unit = {}, isDenied : () -> Unit, isGranted : () -> Unit) {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            Log.d(TAG, "permission Denied " + permission)
+            isDenied()
+        } else {
+            if(ActivityCompat.checkSelfPermission(activity,permission) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "permission Allowed " + permission)
+                isGranted()
+            } else{
+                Log.d(TAG, "permission set to never ask again " + permission);
+                isNeverAskAgain()
+            }
+        }
+    }
+
+    fun checkPermissionsResult(activity : Activity, permissions : Array<String>, grantResults : IntArray,isGranted : () -> Unit, isNeverAskAgain : () -> Unit = {}, isDenied : () -> Unit) {
+        when {
+            grantResults.filter { results -> results ==  PackageManager.PERMISSION_DENIED}.isEmpty() -> {
+                Log.d(TAG,"isGranted()")
+                isGranted()
+            }
+            permissions.filter { permission -> ActivityCompat.shouldShowRequestPermissionRationale(activity, permission) && ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_DENIED }.none() -> {
+                Log.d(TAG,"isNeverAskAgain()")
+                isNeverAskAgain()
+            }
+            grantResults.filter { results -> results ==  PackageManager.PERMISSION_DENIED}.isNotEmpty() -> {
+                Log.d(TAG,"isDenied()")
+                isDenied()
+            }
+        }
+    }
+
+    public fun showRationalDialog(activity : Activity, message : String) {
+        Log.d(TAG,"showRationalDialog($activity,$message")
+        val builder = activity.let { AlertDialog.Builder(it) }
+        builder.setTitle("Manifest Permissions")
+        builder.setMessage(message)
+        builder.setPositiveButton("SETTINGS") { dialog, which ->
+            dialog.dismiss()
+            showAppPermissionSettings(activity)
+        }
+        builder.setNegativeButton("NOT NOW") { dialog, which ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun showAppPermissionSettings(activity : Activity) {
+        Log.d("PermissionsResult", "showAppPermissionSettings()")
+        val intent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", activity.packageName, null)
+        )
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+        activity.startActivityForResult(intent, 1113)
+    }
+}
