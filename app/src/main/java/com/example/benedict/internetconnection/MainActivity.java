371738d2b097cf48fcdc5ff7ed0339b2d79f68df
Benedict <benedictcontawe@gmail.com>
2021-12-16 01:50:25 +0800

    Add new Branch LiveWebViewApp for Communication of WebView and Android View

--- app/src/main/java/com/example/benedict/internetconnection/MainActivity.java
+++ app/src/main/java/com/example/benedict/internetconnection/MainActivity.java
@@ -1,122 +1,110 @@
 package com.example.benedict.internetconnection;
 
-import android.Manifest;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-    private static final int INTERNET_STATE = 0;
-    private TextView txtInternet, txtPing, txtMetered;
+    public static String TAG = MainActivity.class.getSimpleName();
+    private Button btnShowAlert, btnSendToWeb;
+    private EditText edtTextToWeb;
+    private TextView txtFromWeb;
     private MainViewModel viewModel;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtInternet = (TextView) findViewById(R.id.txtInternet);
-        txtPing = (TextView) findViewById(R.id.txtPing);
-        txtMetered = (TextView) findViewById(R.id.txtMetered);
+        btnShowAlert = (Button) findViewById(R.id.button_show_alert);
+        btnSendToWeb = (Button) findViewById(R.id.button_send_to_web);
+        edtTextToWeb = (EditText) findViewById(R.id.edit_text_to_web);
+        txtFromWeb = (TextView) findViewById(R.id.text_from_web);
+        webView = (WebView) findViewById(R.id.web_view);
 
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        setWebView();
         observeData();
     }
 
+    private void setWebView() {
+        viewModel.setWebContentsDebuggingEnabled(getApplicationInfo());
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(viewModel.getWebAppInterface(), WebAppInterface.TAG);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.loadUrl(viewModel.getJavascriptAndroidVersion("called by Android"));
+                view.loadUrl(viewModel.getJavascriptAlert("Hello Web View"));
+
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Log.d("Javascript Alert", "onJsAlert(" + view + ", " + url + ", " + message + ", " + result +")");
+                result.confirm();
+                return viewModel.isUndefined(message) ? true : super.onJsAlert(view, url, message, result);
+            }
+        });
+        webView.loadUrl(viewModel.getHTMLPath());
+    }
+
     private void observeData() {
-        viewModel.observeLiveInternet().observe(this, new Observer<Boolean>() {
+        viewModel.observeLiveText().observe(this, new Observer<String>() {
             @Override
-            public void onChanged(Boolean value) {
-                requestPermissions(viewModel.checkPermission());
-                if (value) {
-                    txtInternet.setText("Network Connection is available");
-                } else {
-                    txtInternet.setText("Network Connection is not available");
-                }
-            }
-        });
-
-        viewModel.observeLivePing().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean value) {
-                requestPermissions(viewModel.checkPermission());
-                if (value) {
-                    txtPing.setText("Google Successfully Ping");
-                    txtMetered.setVisibility(View.VISIBLE);
-                } else {
-                    txtPing.setText("Google Unreachable ping");
-                    txtMetered.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-
-        viewModel.observeLiveMeter().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean value) {
-                if (value) {
-                    txtMetered.setText("Network Connection is Metered");
-                } else {
-                    txtMetered.setText("Network Connection is not Metered");
-                }
+            public void onChanged(String value) {
+                txtFromWeb.setText(value);
             }
         });
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        try {
-            viewModel.unregisterConnectivity();
-        } catch (Exception ex) {
-            showAppPermissionSettings();
+    public void onClick(View view) { Log.d(TAG,"onClick()");
+        if(view.getId() == btnShowAlert.getId() && viewModel.isGreaterThanEqualKitKat()) {
+            webView.evaluateJavascript(viewModel.getJavascriptAlert(edtTextToWeb.getText().toString()), null);
+        } else if(view.getId() == btnShowAlert.getId() && viewModel.isNotGreaterThanEqualKitKat()) {
+            webView.loadUrl(viewModel.getJavascriptAlert(edtTextToWeb.getText().toString()));
+        } else if (view.getId() == btnSendToWeb.getId() && viewModel.isGreaterThanEqualKitKat()) {
+            webView.evaluateJavascript(viewModel.getJavascriptParagraph(edtTextToWeb.getText().toString()), null);
+        } else if (view.getId() == btnSendToWeb.getId() && viewModel.isNotGreaterThanEqualKitKat()) {
+            webView.loadUrl(viewModel.getJavascriptParagraph(edtTextToWeb.getText().toString()));
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        requestPermissions(viewModel.checkPermission());
-        try {
-            viewModel.registerConnectivity();
-            viewModel.checkConnections();
-        } catch (Exception ex) {
-            showAppPermissionSettings();
-        }
+        btnShowAlert.setOnClickListener(this);
+        btnSendToWeb.setOnClickListener(this);
     }
 
-    private void requestPermissions(boolean permissionGranted) {
-        if (!permissionGranted) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{
-                            Manifest.permission.INTERNET,
-                            Manifest.permission.ACCESS_WIFI_STATE,
-                            Manifest.permission.ACCESS_NETWORK_STATE,
-                    },
-                    INTERNET_STATE
-            );
-        }
-    }
-
-    private void showAppPermissionSettings() {
-        Toast.makeText(this,"Internet Permissions Disabled",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.fromParts("package", getPackageName(), null));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        btnShowAlert.setOnClickListener(null);
+        btnSendToWeb.setOnClickListener(null);
     }
 
     @Override
     protected void onDestroy() {
+        webView.removeJavascriptInterface(WebAppInterface.TAG);
         super.onDestroy();
     }
 }
--- app/src/main/java/com/example/benedict/internetconnection/MainViewModel.java
+++ app/src/main/java/com/example/benedict/internetconnection/MainViewModel.java
@@ -1,201 +1,80 @@
 package com.example.benedict.internetconnection;
 
-import android.Manifest;
 import android.app.Application;
-import android.content.Context;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
-import android.net.NetworkRequest;
+import android.content.pm.ApplicationInfo;
 import android.os.Build;
-import android.util.Log;
+import android.webkit.WebView;
 
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import java.io.IOException;
 
 public class MainViewModel extends AndroidViewModel {
 
     private static final String TAG = MainViewModel.class.getSimpleName();
-    private ConnectivityManager connectivityManager;
-    private ConnectivityManager.NetworkCallback networkCallback;
-    private final NetworkReceiver networkReceiver;
-    private final MutableLiveData<Boolean> liveInternet = new MutableLiveData<>();
-    private final MutableLiveData<Boolean> livePing = new MutableLiveData<>();
-    private final MutableLiveData<Boolean> liveMeter = new MutableLiveData<>();
+    private WebAppInterface webAppInterface;
 
     public MainViewModel(Application application) {
         super(application);
-        connectivityManager = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
-        networkReceiver = new NetworkReceiver(this);
+        webAppInterface = new WebAppInterface(application);
     }
-    //region Register Unregister Internet Detection Callback
-    public void registerConnectivity() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            setNetworkCallback();
-            connectivityManager.registerDefaultNetworkCallback(networkCallback);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setNetworkCallback();
-            connectivityManager.registerNetworkCallback(setNetworkRequest(),networkCallback);
-        } else {
-            getApplication().registerReceiver(networkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+    public WebAppInterface getWebAppInterface() {
+        return webAppInterface;
+    }
+
+    public String getHTMLPath() {
+        return "file:///android_asset/index.html";
+    }
+
+    public Boolean isGreaterThanEqualKitKat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+
+    public Boolean isNotGreaterThanEqualKitKat() {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT;
+    }
+
+    public Boolean isNotZeroFlags(ApplicationInfo applicationInfo) {
+        return 0 != (applicationInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE);
+    }
+
+    public Boolean isWebContentsDebuggingEnabled(ApplicationInfo applicationInfo) {
+        return isGreaterThanEqualKitKat() && isNotZeroFlags(applicationInfo);
+    }
+
+    public void setWebContentsDebuggingEnabled(ApplicationInfo applicationInfo) {
+        if (isWebContentsDebuggingEnabled(applicationInfo)) {
+            WebView.setWebContentsDebuggingEnabled(true);
         }
     }
 
-    public void unregisterConnectivity() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            connectivityManager.unregisterNetworkCallback(networkCallback);
-        } else {
-            getApplication().unregisterReceiver(networkReceiver);
-        }
-    }
-    //endregion
-    //region ConnectivityManager initialization of NetworkCallback and NetworkRequest
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void setNetworkCallback() {
-        networkCallback = new ConnectivityManager.NetworkCallback() {
-            @Override
-            public void onAvailable(Network network) {
-                super.onAvailable(network);
-                Log.d(TAG,"onAvailable(" + network + ")");
-                checkConnections();
-            }
-
-            @Override
-            public void onUnavailable() {
-                super.onUnavailable();
-                Log.d(TAG,"onUnavailable()");
-                checkConnections();
-            }
-
-            @Override
-            public void onLost(Network network) {
-                super.onLost(network);
-                Log.d(TAG,"onLost(" + network + ")");
-                checkConnections();
-            }
-
-            @Override
-            public void onCapabilitiesChanged(Network network,NetworkCapabilities networkCapabilities) {
-                super.onCapabilitiesChanged(network, networkCapabilities);
-                if (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)) {
-                    Log.d(TAG,"Serve Higher Quality Content");
-                    liveMeter.postValue(true);
-                } else  {
-                    Log.d(TAG,"Serve Lower Quality Content");
-                    liveMeter.postValue(false);
-                }
-            }
-        };
+    public String getJavascriptAlert(String alert) {
+        return webAppInterface.getJavascriptAlert(alert);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private NetworkRequest setNetworkRequest() {
-        return new NetworkRequest.Builder()
-                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                .build();
-    }
-    //endregion
-    //region LiveData Observers
-    public LiveData<Boolean> observeLiveInternet() {
-        return liveInternet;
+    public String getJavascriptAndroidVersion(String alert) {
+        return webAppInterface.getJavascriptAndroidVersion(alert);
     }
 
-    public LiveData<Boolean> observeLivePing() {
-        return livePing;
+    public String getJavascriptParagraph(String alert) {
+        return webAppInterface.getJavascriptParagraph(alert);
     }
 
-    public LiveData<Boolean> observeLiveMeter() {
-        return liveMeter;
-    }
-    //endregion
-    public void checkConnections() {
-        liveInternet.postValue(hasInternet());
-        livePing.postValue(pingGoogle());
-    }
-    //region Ping methods for checking of internet and ping google
-    private boolean hasInternet() {
-        boolean hasWifi = false;
-        boolean hasMobileData = false;
-        NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
-        for (NetworkInfo info : networkInfos){
-            if (info.getTypeName().equalsIgnoreCase("WIFI"))
-                if (info.isConnected())
-                    hasWifi = true;
-            if (info.getTypeName().equalsIgnoreCase("MOBILE"))
-                if (info.isConnected())
-                    hasMobileData = true;
-        }
-        Log.d(TAG,"hasInternet() : " + String.valueOf(hasMobileData || hasWifi));
-        return hasMobileData || hasWifi;
+    public Boolean isUndefined(String message) {
+        return message.equals("undefined");
     }
 
-    private boolean pingGoogle() {
-        String command = "ping -c 1 google.com";
-        try {
-            return Runtime.getRuntime().exec(command).waitFor() == 0;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    //endregion
-    //region Manifest Permissions
-    public boolean checkPermission() {
-        boolean value = false;
-        Log.d(TAG,"checkPermission()");
-        if (isInternetPermissionGranted()) {
-            Log.d(TAG,"isInternetPermissionGranted()");
-            value = true;
-        }
-        if (isAccessWifiStatePermissionGranted()) {
-            Log.d(TAG,"isAccessWifiStatePermissionGranted()");
-            value = true;
-        }
-        if (isAccessNetworkStatePermissionGranted()) {
-            Log.d(TAG,"isAccessNetworkStatePermissionGranted()");
-            value = true;
-        }
-        return value;
+    public LiveData<String> observeLiveText() {
+        if (webAppInterface.observeLiveText() != null) return webAppInterface.observeLiveText();
+        else return new MutableLiveData<String>("null");
     }
 
-    private boolean isInternetPermissionGranted() {
-        return ActivityCompat
-                .checkSelfPermission(
-                        getApplication(),
-                        Manifest.permission.INTERNET
-                ) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private boolean isAccessWifiStatePermissionGranted() {
-        return ActivityCompat
-                .checkSelfPermission(
-                        getApplication(),
-                        Manifest.permission.ACCESS_WIFI_STATE
-                ) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private boolean isAccessNetworkStatePermissionGranted() {
-        return ActivityCompat
-                .checkSelfPermission(
-                        getApplication(),
-                        Manifest.permission.ACCESS_NETWORK_STATE
-                ) == PackageManager.PERMISSION_GRANTED;
-    }
-    //endregion
     @Override
     protected void onCleared() {
+        webAppInterface = null;
         super.onCleared();
-        connectivityManager = null;
     }
 }
+//https://medium.com/mobile-app-development-publication/making-android-interacting-with-web-app-921be14f99d8
+//https://www.codexpedia.com/android/javascript-interface-for-android-and-javascript-communication/
