1dcdcb1c2e84d5ded47bf5696fdddd9ee88c0d0f
Benedict Contawe <benedictcontawe@gmail.com>
2022-06-13 12:13:19 +0800

    Update J Unit Testing

--- app/src/test/java/com/example/connectionapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/connectionapp/ExampleUnitTest.kt
@@ -11,7 +11,16 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun checkList_isFirstRepeatedValues() {
+        var letters : List<String> = listOf<String>("A","B","C","A","B","D") //println("Distinct ${letters.distinct().firstOrNull()}")
+        println("First ${letters.groupingBy { it }.eachCount().filter { it.value > 1 }.firstNotNullOfOrNull { it.key }}")
+
+        letters = listOf<String>("X","Y","Z","A","B","Y")
+        println("Second ${letters.groupingBy { it }.eachCount().filter { it.value > 1 }.firstNotNullOfOrNull { it.key }}")
+
+        letters = listOf<String>("A","B","C","D","E","F")
+        println("Third ${letters.groupingBy { it }.eachCount().filter { it.value > 1 }.firstNotNullOfOrNull { it.key }}")
+
+        assert(true)
     }
 }
