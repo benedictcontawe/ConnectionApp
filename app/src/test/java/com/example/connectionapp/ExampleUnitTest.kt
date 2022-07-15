98717159ca0966016c560ef6cfd49642522a0218
Benedict Contawe <benedict.contawe@afpayments.com>
2022-07-15 12:49:05 +0800

    Update some code

--- app/src/test/java/com/example/connectionapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/connectionapp/ExampleUnitTest.kt
@@ -1,5 +1,9 @@
 package com.example.connectionapp
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.junit.Test
 import org.junit.Assert.*
 /**
@@ -20,5 +24,24 @@ class ExampleUnitTest {
         println("Third ${letters.groupingBy { it }.eachCount().filter { it.value > 1 }.firstNotNullOfOrNull { it.key }}")
 
         assert(true)
+    }
+
+    @Test
+    suspend fun channel() : Unit = coroutineScope {
+        val channel = Channel<Int>()
+        launch {
+            repeat(5) { index ->
+                delay(1000)
+                println("Producing next one")
+                channel.send(index * 2)
+            }
+        }
+
+        launch {
+            repeat(5) {
+                val received = channel.receive()
+                println(received)
+            }
+        }
     }
 }
