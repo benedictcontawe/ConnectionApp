78728f274c09aa1f63b39530ff76c1477340f7b5
Benedict Contawe <benedict.contawe@afpayments.com>
2022-09-12 11:04:08 +0800

    Update Manifest Permission

--- app/src/main/java/com/example/benedict/permission/ManifestPermission.kt
+++ app/src/main/java/com/example/benedict/permission/ManifestPermission.kt
@@ -29,16 +29,19 @@ object ManifestPermission {
     val allPermissions =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             arrayOf(
+                Manifest.permission.CAMERA,
                 Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.READ_SMS,
                 Manifest.permission.READ_CONTACTS,
                 Manifest.permission.WRITE_CONTACTS,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_PHONE_NUMBERS
+                Manifest.permission.READ_PHONE_NUMBERS,
+                Manifest.permission.RECORD_AUDIO
             )
         } else {
             arrayOf(
+                Manifest.permission.CAMERA,
                 Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.READ_SMS,
                 Manifest.permission.READ_CONTACTS,
