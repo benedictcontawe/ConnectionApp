08c5421e6fe03de637213dee223b1da5d9807982
Benedict Contawe <benedictcontawe@gmail.com>
2023-12-20 11:17:53 +0800

    Add some condition statement for intent

--- app/src/main/java/com/example/nfcapp/MainActivity.java
+++ app/src/main/java/com/example/nfcapp/MainActivity.java
@@ -12,6 +12,7 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
@@ -46,7 +47,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() { Log.d(TAG,"onResume()");
         super.onResume();
         viewModel.checkNFCStatus();
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        final PendingIntent pendingIntent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE);
+        } else {
+            pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        }
         if (nfcAdapter != null) //enabling foreground dispatch for getting intent from NFC event:
             nfcAdapter.enableForegroundDispatch(this, pendingIntent, viewModel.getIntentFilter(), viewModel.techList);
         if (getIntent() != null)
